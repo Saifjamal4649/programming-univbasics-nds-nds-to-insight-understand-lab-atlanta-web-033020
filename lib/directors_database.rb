10cc864797b0bc90a75cba18e05a494739b1e5db
Saif Jamal <saifjamal4649@gmail.com>
2020-02-11 03:18:46 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -5,12 +5,20 @@ def directors_database
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
   
 end
-require 'pp'
-d
+require 'directors_database'
+# Call the method directors_database to retrieve the NDS
 
-
+directors_database
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   nil
-  pp nds
-en
+ pp nds
+end
+
+def print_first_directors_movie_titles
+  index = 0
+  while index < directors_database[0][:movies].length do
+    puts directors_database[0][:movies][index][:title]
+    index += 1
+  end
+end
