12bae15eb74de477a9637d79dc4edd8c0fff98da
Saif Jamal <saifjamal4649@gmail.com>
2020-02-11 03:17:28 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -13,17 +13,3 @@ def pretty_print_nds(nds)
   pp nds
 end
 
-# printing directors_database
-pretty_print_nds(directors_database)
-
-def print_first_directors_movie_titles
-  movies_ar = directors_database[0][:movies]
-  movies_ar_index = 0
-  while movies_ar_index < movies_ar.count do
-    puts movies_ar[movies_ar_index][:title]
-    movies_ar_index += 1 
-  end 
-end
-
-# checking the result
-print_first_directors_movie_titles
